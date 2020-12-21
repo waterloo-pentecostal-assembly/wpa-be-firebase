a9248c7da029543ee84d3c59e00c099dd73ba82e
Joshua Ramsamooj <joshua.ramsamooj@gmail.com>
2020-12-21 16:36:09 -0500

    cleanup

--- functions/src/config/config.js
+++ functions/src/config/config.js
@@ -13,10 +13,7 @@ exports.getConfig = (env) => {
 
         // Use environment variable is it exists
         if (process.env.SERVICE_ACCOUNT_CREDENTIALS) {
-            console.log('!!!!!!!! USING SERVICE_ACCOUNT_CREDENTIALS');
             serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_CREDENTIALS); 
-            console.log(serviceAccount);
-            console.log(typeof(serviceAccount));
         } else if (fs.existsSync(serviceAccountFile)) {
             serviceAccount = path.resolve(__dirname) + '/service-account-dev.json';
         } else {
