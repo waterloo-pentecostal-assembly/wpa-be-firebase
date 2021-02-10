83740b9a97373f902d1acc377b3a2830e9835919
YiJie (Jackie) Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-02-10 10:54:15 -0500

    finished streaks function

--- functions/src/updates/index.js
+++ functions/src/updates/index.js
@@ -1,2 +1,4 @@
 exports.userSnippetUpdates = require('./updateUserSnippet');
 exports.bibleSeriesSnippet = require('./updateBibleSeriesSnippet');
+exports.updateStreaks = require('./updateStreak');
+
