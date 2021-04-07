ba53d96189f0d468a8f7c530c9655cdf82d50bc7
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-04-07 13:33:00 -0400

    updates progress on bibles series update

--- functions/src/updates/index.js
+++ functions/src/updates/index.js
@@ -2,3 +2,4 @@ exports.userSnippetUpdates = require('./updateUserSnippet');
 exports.bibleSeriesSnippet = require('./updateBibleSeriesSnippet');
 exports.updateProgress = require('./updateProgress');
 exports.deleteUser = require('./deleteUser');
+exports.progressReset = require('./progressReset');
---
+++ functions/src/updates/progressReset.js
@@ -0,0 +1,31 @@
+const { firestore, functions } = require('../index');
+
+exports.progressReset = functions
+    .firestore
+    .document('/bible_series/{bibleSeriesId}')
+    .onUpdate( async (change, context) => {
+        const newData = change.after.data();
+        const oldData = change.before.data();
+        
+        if(newData['is_active'] && !oldData['is_active']){
+            const bibleSeriesId = change.after.id;
+            const achievementsSnapshot = await firestore
+                .collection('achievements')
+                .get();
+
+            achievementsSnapshot.forEach(async (doc) => {
+                await firestore.collection('achievements').doc(doc.id).update({'series_progress': 0});
+            });
+
+            const bibleSeriesSnapshot = await firestore
+                .collection('bible_series')
+                .get();
+
+            bibleSeriesSnapshot.forEach(async (doc) => {
+                if(doc.id != bibleSeriesSnapshot){
+                    await firestore.collection('bible_series').doc(doc.id).update({'is_active': false});
+                }
+            });
+        }
+        return;
+    });
